6ca5e568b1b762e147f879581e375c90c319a911
Ade Wija Nugraha <secretaway.awn@gmail.com>
2019-05-27 14:49:10 +0700

    add project configuration

--- src/main/java/com/awn/unittestscanner/UnitTestScannerApplication.java
+++ src/main/java/com/awn/unittestscanner/UnitTestScannerApplication.java
@@ -7,16 +7,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.io.File;
 
 @SpringBootApplication
-public class UnitTestScannerApplication {
+public class UnitTestScannerApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		new File(ProjectController.UPLOAD_DIRECTORY).mkdirs();
 		new File(ProjectController.UPLOAD_DIRECTORY + "/projectDirectories/").mkdirs();
+
 		SpringApplication.run(UnitTestScannerApplication.class, args);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/projectDirectories/**").addResourceLocations("file:ext-resources/").setCachePeriod(0);
 	}
 
 }
